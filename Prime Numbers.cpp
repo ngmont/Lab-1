321b5519c5ea5daeabd6ae281ffd0ef08ed85cf6
ngmont <16mawise@gmail.com>
2016-05-18 15:31:38 -0400

    Update Prime Numbers.cpp

--- Prime Numbers.cpp
+++ Prime Numbers.cpp
@@ -8,7 +8,22 @@
  *
  * BEGIN Calculating Prime Numbers
  * Input Nuber
- * 
+ * WHILE (Num != QUIT)
+ *     Create bool called isPrime is equal to true
+ *     FOR (Count starting at 2; Count <= Num halfs; incri. count)
+ *        IF (Num modulus 2 is equal to zero)
+ *        Set isPrime equal to false
+ *        END IF
+ *     END FOR 
+ *     IF(isPrime)
+ *        Answer is Prime
+ *        Output answer
+ *     ELSE
+ *        Answer is not Prime
+ *        Output answer
+ *        END IF
+ *     Input new Number
+ * END WHILE
  *END Calculating Prime Numbers
 *********************************************************************/
 
@@ -31,36 +46,54 @@ int main()
     string Ans;               // changed to string
     bool isPrime;
 	/**************************start main program*********************/
-	
+    //Input Number	
     cout << "Enter number or QUIT: ";
     cin  >> Num; 
-    
+    //While Num is not QUIT
     while (Num != QUIT){
+    	//Create bool isPrime equal to true and test it 
         isPrime = true; 
+        //FOR (Count less than Num/2, incriment cout)
         for (Count = 2; Count <= Num/2; Count++){
-             
-             if(Num % Count == 0) {// changed to equality operator
+        	
+             //IF (Num modulus Count is zero)
+             if(Num % Count == 0) {
                //num is not prime
                isPrime = false;
                break;
-               } 
+               } //END IF
               
-        }
-        if (isPrime)
-           Ans = "Num is Prime"; 
-           
-        else
-           Ans = "Num is not Prime"; 
-           cout << "Answer: " << Ans; 
+        }//END FOR
         
+        //IF(answer is isPrime)
+        if (isPrime)
+        {  //Answer is Prime, display answer
+           Ans = "Num is Prime"; 
+           cout << "Answer: " << Ans; 
+        ///Answer is not Prime   
+        }
+        else 
+        {  //Answer is not Prime, display answer
+           Ans = "Num is not Prime"; 
+           cout << "Answer: " << Ans; //END IF
+        }   
+        //Ask user for a new number or QUIT
+        cout << "\n";
         cout << "Enter new number or QUIT"; 
         cin  >> Num; 
         
-    }
-}
+  
+    }//END WHILE  
+
+
+}//END PRIME NUMBER PROGRAM
+
 
         
             
         
+        
+    
+
         
     
